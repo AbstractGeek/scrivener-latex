2e45feadbaab2a242af4d38133abea7974b6d7ce
Dinesh Natesan <AbstractGeek@users.noreply.github.com>
2018-11-06 15:46:45 +0530

    added cropping and separating of figures

--- scrivener_mmd_compile.py
+++ scrivener_mmd_compile.py
@@ -6,6 +6,7 @@ from glob import glob
 import os
 import sys
 import subprocess
+import re
 
 
 def get_file_if_unique(location, ext):
@@ -20,66 +21,151 @@ def get_file_if_unique(location, ext):
         sys.exit()
 
 
-def find_in_texfile(tex_content, matchstr):
+def find_in_texfile(tex_content, matchstr, unique=True):
     """Find matchstr in tex_content list."""
     doc_match = [i for i, line in enumerate(tex_content)
                  if matchstr in line]
-    print(doc_match)
-    if not (len(doc_match) == 1):
-        print('Multiple/No ' + matchstr + ' found in generated tex file.')
-        sys.exit()
-    else:
-        doc_match = doc_match[0]
 
-    return doc_match
+    if unique:
+        if not (len(doc_match) == 1):
+            print('Multiple/No ' + matchstr + ' found in generated tex file.')
+            sys.exit()
+        else:
+            doc_match = doc_match[0]
+        return doc_match
+    else:
+        return doc_match
+
+
+def crop_pdfs(src, dest, margins):
+    """Crop pdfs in src and place in dest."""
+    files = glob(os.path.join(src, '*.pdf'))
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+
+    for pdf in files:
+        _, filename = os.path.split(pdf)
+        subprocess.run(["pdfcrop", "--margins", margins,
+                        os.path.join(src, filename),
+                        os.path.join(dest, filename)])
+
+
+def split_figure_tex(tex_content, dest):
+    """Split figure tex file into individual files."""
+    fig_start = find_in_texfile(tex_content, "\\begin{figure}", False)
+    fig_end = find_in_texfile(tex_content, "\\end{figure}", False)
+    label_lines = find_in_texfile(tex_content, "\\label{", False)
+
+    # Check for consistency in counts
+    if not (len(fig_start) == len(fig_end)) and (
+            len(fig_start) == len(label_lines)):
+
+        print('Unequal begin{figure} (' +
+              str(len(fig_start)) + '), end{figure} ('
+              + str(len(fig_end)) + ') and label ('
+              + str(len(label_lines)) + ') found.')
+        sys.exit()
+
+    else:
+        # Find label name (will become filename)
+        p = re.compile('label{(\S+)}')
+        tex_names = []
+        for line in label_lines:
+            temp = p.search(tex_content[line])
+            if len(temp.groups()) != 1:
+                print('Something is off with label detection')
+                sys.exit()
+            else:
+                tex_names.append(temp.groups()[0])
+
+        # Create folder if it does not exist
+        if not os.path.exists(dest):
+            os.makedirs(dest)
+
+        # Write individual figure tex file
+        for i, tex in enumerate(tex_names):
+            tex_output = tex_content[fig_start[i]:fig_end[i] + 1]
+            # Write complete tex file
+            with open(os.path.join(dest, tex) + '.tex', 'w') as tex_file:
+                tex_file.writelines(tex_output)
+
+
+def parseArguments(args):
+    """Properly parse input arguments."""
+    if not args.bibfile:
+        args.bibfile = get_file_if_unique(args.location, '*.bib')
+    if not args.maintex:
+        args.maintex = get_file_if_unique(args.location, '*main.tex')
+    if not args.outfile:
+        args.outfile = os.path.splitext(args.mmd[0])[0]
+    if not args.figure_source:
+        args.figure_source = os.path.join(args.location, 'figures')
+    if not args.figuretex:
+        args.figuretex = os.path.join(args.figure_source, 'figures.tex')
+    if not args.figure_dest:
+        args.figure_dest = os.path.join(args.figure_source, 'cropped')
+    if not args.tex_folder:
+        args.tex_folder = os.path.join(args.figure_source, 'texs')
+    if not args.whitespace_margins:
+        args.whitespace_margins = '0 20 0 20'
+
+    return args
 
 
 def main():
-    """Parse input and download comic(s)."""
+    """Parse input and generate documents."""
     parser = argparse.ArgumentParser(
         description=(
             'Converts scrivener mmd outputs to docx, tex and pdf. '
             'Integrates it with input and footer tex file, if available.'))
 
     parser.add_argument('mmd', nargs=1, type=str,
-                        help='Multimarkdown file to be converted')
+                        help='multimarkdown file to be converted')
     parser.add_argument(
         "-l", "--location", default=os.getcwd(), help="set working directory")
     parser.add_argument(
         "-b", "--bibfile", default=False,
-        help="Bibtex file used to generate bibliography.")
+        help="bibtex file used to generate bibliography.")
     parser.add_argument(
         "-m", "--maintex", default=False,
-        help="Main tex file that controls appearance.")
+        help="main tex file that controls appearance.")
     parser.add_argument(
         "-o", "--outfile", default=False,
         help="outfile prefix for generated files.")
+    parser.add_argument(
+        "-f", "--figuretex", default=False,
+        help="Tex file containing figure code chunks.")
+    parser.add_argument(
+        "-s", "--figure-source", default=False,
+        help="source folder containing figures.")
+    parser.add_argument(
+        "-d", "--figure-dest", default=False,
+        help="destination folder for cropped figures.")
+    parser.add_argument(
+        "-w", "--whitespace-margins", default=False,
+        help="white space margins for cropped figures.")
+    parser.add_argument(
+        "-t", "--tex-folder", default=False,
+        help="destination folder for individual figure tex files.")
 
-    args = parser.parse_args()
+    args = parseArguments(parser.parse_args())
 
-    # Find bibliography and tex file if not provided
-    if not args.bibfile:
-        bibfile = get_file_if_unique(args.location, '*.bib')
-    else:
-        bibfile = args.bibfile
-
-    if not args.maintex:
-        maintex = get_file_if_unique(args.location, '*main.tex')
-    else:
-        maintex = args.maintex
-
-    if not args.outfile:
-        outfile = os.path.splitext(args.mmd[0])[0]
-    else:
-        outfile = args.outfile
     # Change location to the main folder as the working directory
     os.chdir(args.location)
 
+    # Crop pdfs for figures
+    crop_pdfs(args.figure_source, args.figure_dest, args.whitespace_margins)
+
+    # Split figure tex file
+    with open(args.figuretex, 'r') as tex_file:
+        figure_tex_content = tex_file.readlines()
+    split_figure_tex(figure_tex_content, args.tex_folder)
+
     # Generate docx file from multimarkdown
     subprocess.run(["pandoc", "-s", "-S", "--normalize",
-                    "--bibliography", bibfile, "--toc",
+                    "--bibliography", args.bibfile, "--toc",
                     "-f", "markdown", "-t", "docx",
-                    "-o", outfile + ".docx", args.mmd[0]])
+                    "-o", args.outfile + ".docx", args.mmd[0]])
 
     # Generate temp tex file
     subprocess.run(["pandoc", "-s", "-S", "--normalize",
@@ -90,7 +176,7 @@ def main():
     # Read temp, main tex file
     with open('scrivener_mmd_compile_temp.tex', 'r') as tex_file:
         input_tex_content = tex_file.readlines()
-    with open(maintex, 'r') as tex_file:
+    with open(args.maintex, 'r') as tex_file:
         main_tex_content = tex_file.readlines()
 
     # find start, input and stop in tex files
@@ -105,14 +191,14 @@ def main():
         main_tex_content[input_line + 1:]
 
     # Write complete tex file
-    with open(outfile + '.tex', 'w') as tex_file:
+    with open(args.outfile + '.tex', 'w') as tex_file:
         tex_file.writelines(complete_tex_file)
 
     # Compile tex and bib files
-    subprocess.run(["pdflatex", outfile + '.tex'])
-    subprocess.run(["bibtex", outfile + '.aux'])
-    subprocess.run(["pdflatex", outfile + '.tex'])
-    subprocess.run(["pdflatex", outfile + '.tex'])
+    subprocess.run(["pdflatex", args.outfile + '.tex'])
+    subprocess.run(["bibtex", args.outfile + '.aux'])
+    subprocess.run(["pdflatex", args.outfile + '.tex'])
+    subprocess.run(["pdflatex", args.outfile + '.tex'])
 
     # Print exit message
     print("Scrivener mmd has been exported as docx, tex and pdf")
